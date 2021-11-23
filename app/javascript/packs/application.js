0fc9eaa3105bcd536babaeff1cfe443569297dbd
Olly Headey <olly@headey.org>
2021-11-23 23:10:07 +0000

    Button styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,6 @@
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-//import "@rails/ujs"
-//import "@rails/activestorage"
-
 window.Stimulus = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 Stimulus.load(definitionsFromContext(context))
