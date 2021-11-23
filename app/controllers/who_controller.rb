0c96a23367fde9c8093a77b1fb1560e972d1c6e0
Olly Headey <olly@headey.org>
2021-11-23 17:11:28 +0000

    Ensure all people returned have an attached avatar image

--- app/controllers/who_controller.rb
+++ app/controllers/who_controller.rb
@@ -5,11 +5,14 @@ class WhoController < ApplicationController
   end
 
   def play
-    @person = unless params[:recent].present?
-        Person.all.with_attached_avatar.sample
+    scope = unless params[:recent].present?
+        Person.with_attached_avatar.all
       else
-        Person.where("trello_created_at > ?", 1.year.ago).with_attached_avatar.sample
+        Person.where("trello_created_at > ?", 1.year.ago)
       end
+
+    @person = scope.with_attached_avatar.select {|p| p.avatar.attached?}.sample
+
     names = @person.name.split(" ")
     @masked_name = names.map {|name| name.gsub(/\B[\w]/, "*") }.join(" ")
   end
